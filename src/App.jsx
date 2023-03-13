ed3c826f98aa3de0483881fd5eb6609904e901f4
Jake Samuels <samuels242@googlemail.com>
2023-03-13 21:23:22 +0000

    Import Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Header from "./components/shared/Header";
+
 const App = () => {
   return (
     <div>
-      
+      <Header />
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
