353511ea6665b55df58f0cd370028a6bb6cff4ff
Jake Samuels <samuels242@googlemail.com>
2023-03-13 22:23:20 +0000

    Import Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/shared/Header";
+
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
