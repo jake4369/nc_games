f0a0f902ceb2276bf756a8b388f14d42c89f9497
Jake Samuels <samuels242@googlemail.com>
2023-03-14 19:46:56 +0000

    Use IsLoadedProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { IsLoadedProvider } from "./contexts/IsLoadedContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <IsLoadedProvider>
+        <App />
+      </IsLoadedProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
