98b414e64749f09aef3433c46b2d4f84c1cf6b22
Jake Samuels <samuels242@googlemail.com>
2023-03-14 12:46:44 +0000

    Latest reviews component

---
+++ src/components/LatestReviews.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { getLatestReviews } from "../utils/api";
+
+const LatestReviews = () => {
+  const [latestReviews, setLatestReviews] = useState([]);
+
+  useEffect(() => {
+    getLatestReviews().then((data) => {
+      const mostRecentReviews = data.slice(0, 3);
+      setLatestReviews(mostRecentReviews);
+    });
+  }, []);
+
+  const latestReviewCards = latestReviews.map((review, index) => {
+    return (
+      <div className="latest-review-card" key={review.review_id}>
+        <img
+          src={review.review_img_url}
+          alt=""
+          className="latest-review-card__img"
+        />
+
+        <div className="latest-review-card__text-content">
+          <div className="latest-review-card__number">0{index + 1}</div>
+          <h3 className="latest-review-card__heading">{review.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <section className="latest-reviews">
+      <h2 className="latest-reviews__heading">Latest Reviews</h2>
+
+      <div className="latest-reviews__container">{latestReviewCards}</div>
+
+      <button className="view-all-reviews-btn">View All</button>
+    </section>
+  );
+};
+
+export default LatestReviews;
