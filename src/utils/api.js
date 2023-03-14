b912c8e178e9be7c7778607ed81b092c5d801f33
Jake Samuels <samuels242@googlemail.com>
2023-03-14 09:11:25 +0000

    Add getReviews util function

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,12 @@ const reviewsApi = axios.create({
   baseURL: "https://nc-games-api-yf5t.onrender.com/api",
 });
 
+export const getCategories = () => {
+  return reviewsApi.get("/categories").then(({ data }) => {
+    return data.categories;
+  });
+};
+
 export const getReviews = () => {
   return reviewsApi.get("/reviews").then(({ data }) => {
     return data.reviews;
