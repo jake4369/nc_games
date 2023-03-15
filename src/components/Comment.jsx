eb98859d16a172b82d1784e43df7b949616531b1
Jake Samuels <samuels242@googlemail.com>
2023-03-15 14:13:12 +0000

    Create Comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,10 +3,10 @@ const Comment = ({ userAvatar, author, body, createdAt }) => {
     <div className="comment-card">
       <div className="comment-card__author-info">
         <img src={userAvatar} alt="" className="comment-card__author-avatar" />
-        <p className="comment-card__author">{author}</p>
+        <p className="comment-card__author-name">{author}</p>
       </div>
       <p className="comment-card__body">{body}</p>
-      <p className="comment-card__date">{createdAt}</p>
+      <p className="comment-card__date">{createdAt.split("T")[0]}</p>
     </div>
   );
 };
