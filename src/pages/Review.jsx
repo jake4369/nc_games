ad5f537e87e8b39839e5af4a96ce37c03de6a650
Jake Samuels <samuels242@googlemail.com>
2023-03-15 08:55:04 +0000

    Add content for single review

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,8 +1,25 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { getReviews, getUser } from "../utils/api";
 
 const Review = () => {
   const [singleReview, setSingleReview] = useState({});
+  const [user, setUser] = useState({});
+  const params = useParams();
+
+  useEffect(() => {
+    getReviews()
+      .then((data) => {
+        const review = data.find((obj) => obj.review_id === params.id * 1);
+        setSingleReview(review);
+        return getUser(review.owner);
+      })
+      .then((data) => {
+        setUser(data);
+      });
+  }, []);
+
+  console.log(singleReview);
 
   return (
     <div className="review-page">
@@ -10,11 +27,19 @@ const Review = () => {
         <div className="vote-counter__container"></div>
 
         <div className="single-review-card__text-content">
-          <div className="single-review-card__author-info">
-            <img src="" alt="" className="author-avatar" />
-            <div className="single-review-card__date"></div>
+          <div className="single-review-card__owner-info">
+            <img src={user.avatar_url} alt="" className="owner-avatar" />
+            <p className="single-review-card__owner-name">
+              {singleReview.owner}
+            </p>
+            <div className="single-review-card__date">
+              {singleReview.created_at.split("T")[0]}
+            </div>
           </div>
-          <p className="single-review-card__review-body"></p>
+          <h2 className="single-review-card__title">{singleReview.title}</h2>
+          <p className="single-review-card__review-body">
+            {singleReview.review_body}
+          </p>
         </div>
       </div>
     </div>
