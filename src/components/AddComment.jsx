e7dc1d8e8ef79b0cb08df926a0535f75172399dd
Jake Samuels <samuels242@googlemail.com>
2023-03-16 16:47:11 +0000

    Add AddComment component

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { addComment } from "../utils/api";
+
+const Comment = ({ reviewId, setComments, handleNewComment }) => {
+  const [comment, setComment] = useState({
+    review_id: Number(reviewId),
+    author: localStorage.getItem("loggedInUser")
+      ? JSON.parse(localStorage.getItem("loggedInUser")).username
+      : "",
+    body: "",
+  });
+
+  const handleChange = (e) => {
+    setComment((prevState) => {
+      return {
+        ...prevState,
+        body: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addComment(comment.review_id, comment.author, comment.body).then(
+      (response) => {
+        console.log(response);
+        setComments((prevState) => [...prevState, response]);
+        handleNewComment(response);
+      }
+    );
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        name="comment"
+        className="comment-textarea"
+        placeholder="Add a comment..."
+        value={comment.body}
+        onChange={handleChange}
+      ></textarea>
+      <button className="send-comment-btn">SEND</button>
+    </form>
+  );
+};
+
+export default Comment;
