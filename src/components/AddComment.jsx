ae5ac8eb3f1054699c0a9d90b6388b3e999d81a3
Jake Samuels <samuels242@googlemail.com>
2023-03-16 20:02:03 +0000

    improve AddComment component

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,46 +1,63 @@
 import { useState } from "react";
 import { addComment } from "../utils/api";
+import { forwardRef } from "react";
 
-const Comment = ({ reviewId, setComments, handleNewComment }) => {
-  const [comment, setComment] = useState({
-    review_id: Number(reviewId),
-    author: localStorage.getItem("loggedInUser")
-      ? JSON.parse(localStorage.getItem("loggedInUser")).username
-      : "",
-    body: "",
-  });
-
-  const handleChange = (e) => {
-    setComment((prevState) => {
-      return {
-        ...prevState,
-        body: e.target.value,
-      };
+const Comment = forwardRef(
+  ({ reviewId, setComments, handleNewComment }, ref) => {
+    const [comment, setComment] = useState({
+      review_id: Number(reviewId),
+      author: localStorage.getItem("loggedInUser")
+        ? JSON.parse(localStorage.getItem("loggedInUser")).username
+        : "",
+      body: "",
     });
-  };
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addComment(comment.review_id, comment.author, comment.body).then(
-      (response) => {
-        setComments((prevState) => [...prevState, response]);
-        handleNewComment(response);
+    const handleChange = (e) => {
+      setComment((prevState) => {
+        return {
+          ...prevState,
+          body: e.target.value,
+        };
+      });
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (comment.body !== "") {
+        setIsSubmitting(true);
+        addComment(comment.review_id, comment.author, comment.body).then(
+          (response) => {
+            setComments((prevState) => [...prevState, response]);
+            handleNewComment(response);
+            setIsSubmitting(false);
+            setComment((prevState) => {
+              return {
+                ...prevState,
+                body: "",
+              };
+            });
+          }
+        );
       }
-    );
-  };
+    };
 
-  return (
-    <form onSubmit={handleSubmit} className="add-comment-form">
-      <textarea
-        name="comment"
-        className="comment-textarea"
-        placeholder="Add a comment... Max 255 Characters"
-        value={comment.body}
-        onChange={handleChange}
-      ></textarea>
-      <button className="send-comment-btn">SEND</button>
-    </form>
-  );
-};
+    return (
+      <form onSubmit={handleSubmit} className="add-comment-form">
+        <textarea
+          name="comment"
+          className="comment-textarea"
+          placeholder="Add a comment... Max 255 Characters"
+          value={comment.body}
+          onChange={handleChange}
+          ref={ref}
+        ></textarea>
+        <button className="send-comment-btn" disabled={isSubmitting}>
+          {isSubmitting ? "..." : "SEND"}
+        </button>
+      </form>
+    );
+  }
+);
 
 export default Comment;
