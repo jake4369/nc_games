e5d25c89a0dbb9497dfea05ec4d0a5d5e6af7853
Jake Samuels <samuels242@googlemail.com>
2023-03-17 09:28:56 +0000

    Add delete functionality to comments of logged in user

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,47 @@
-const Comment = ({ userAvatar, author, body, createdAt }) => {
+import { useContext, useState, useEffect } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { deleteComment } from "../utils/api";
+import { FaTrash } from "react-icons/fa";
+import BouncingDotsLoader from "./shared/BouncingDotsLoader";
+
+const Comment = ({
+  userAvatar,
+  author,
+  body,
+  createdAt,
+  commentId,
+  onDelete,
+}) => {
+  const { loggedInUser } = useContext(UserContext);
+  const [isDeleted, setIsDeleted] = useState(false);
+  const [viewTrashIcon, setViewTrashIcon] = useState(true);
+
+  const handleDelete = () => {
+    setIsDeleted(false);
+    setViewTrashIcon(false);
+
+    deleteComment(commentId).then((response) => {
+      console.log(response);
+      setIsDeleted(true);
+      setViewTrashIcon(true);
+      onDelete(commentId);
+    });
+  };
+
   return (
     <div className="comment-card">
       <div className="comment-card__author-info">
         <img src={userAvatar} alt="" className="comment-card__author-avatar" />
         <p className="comment-card__author-name">{author}</p>
+        {loggedInUser.username === author && (
+          <>
+            {viewTrashIcon ? (
+              <FaTrash className="trash-icon" onClick={handleDelete} />
+            ) : (
+              <BouncingDotsLoader />
+            )}
+          </>
+        )}
       </div>
       <p className="comment-card__body">{body}</p>
       <p className="comment-card__date">{createdAt.split("T")[0]}</p>
